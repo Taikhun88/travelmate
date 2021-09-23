80258e8b6df5968d8571e66ef83ac7f1ed8b804e
Benjamin Gleitz <gleitz.benjamin@gmail.com>
2021-09-23 17:05:00 +0200

    feat : search by city --DOING

--- travel-mate-back/src/Controller/SearchController.php
+++ travel-mate-back/src/Controller/SearchController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Repository\CityRepository;
+use App\Repository\CountryRepository;
 use App\Repository\EventRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -13,15 +15,24 @@ class SearchController extends AbstractController
     /**
      * @Route("/search", name="search")
      */
-    public function index(EventRepository $eventRepository, Request $request): Response
+    public function index(CityRepository $cityRepository, EventRepository $eventRepository, Request $request): Response
     {
+        $citiesList = $cityRepository->findAll();
 
         // 1) On récupère le mot-clé saisi dans le formulaire de recherche
         $query = $request->query->get('search');
-        dd($query);
+        // dd($query);
+
+        // 2) On récupère toutes les séries qui contiennent ce mot-clé
+        // $results = $eventRepository->searchEventByCity($query);
+        // dd($results);
+
+
 
         return $this->render('search/index.html.twig', [
-            'controller_name' => 'SearchController',
+            // 'results' => $results,
+            'query' => $query,
+            'cities' => $citiesList
         ]);
     }
 }
--- travel-mate-back/src/Repository/EventRepository.php
+++ travel-mate-back/src/Repository/EventRepository.php
@@ -19,6 +19,26 @@ class EventRepository extends ServiceEntityRepository
         parent::__construct($registry, Event::class);
     }
 
+    /**
+     * Effectue une recherche d'evenement en fonction de la variable
+     * $city
+     * 
+     * Version 1 : Query builder
+     *
+     * @param $title
+     * @return Event[]
+     */
+    public function searchEventByCity($city)
+    {
+        // https://www.doctrine-project.org/projects/doctrine-orm/en/2.9/reference/query-builder.html
+        return $this->createQueryBuilder('event')
+            // Clause WHERE pour filtre en fonction de $city
+            ->where('event.city LIKE :city')
+            ->setParameter(':city', "%$city%")
+            ->getQuery()
+            ->getResult();
+    }
+
     // /**
     //  * @return Event[] Returns an array of Event objects
     //  */
