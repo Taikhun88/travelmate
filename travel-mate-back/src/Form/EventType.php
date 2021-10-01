e7a359d640504071e87d407ef58208bf9096a425
Tai KHUN <taiyang78190@gmail.com>
2021-10-01 10:28:29 +0200

    feat : optimisation formtype --DOING

--- travel-mate-back/src/Form/EventType.php
+++ travel-mate-back/src/Form/EventType.php
@@ -3,6 +3,7 @@
 namespace App\Form;
 
 use App\Entity\Event;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
@@ -50,7 +51,11 @@ class EventType extends AbstractType
             //->add('updatedAt')
             //->add('users')
             //->add('categories')
-            ->add('city')
+            ->add('city', EntityType::class,[
+                'class' => Event::class,
+                'choices' 
+
+            ])
             //->add('creator')
         ;
     }
--- travel-mate-back/src/Form/UserType.php
+++ travel-mate-back/src/Form/UserType.php
@@ -5,7 +5,6 @@ namespace App\Form;
 use App\Entity\User;
 use DateTimeInterface;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\ChoiceList\ChoiceList;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
